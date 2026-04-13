70ece90f96b788a73c73e2f737bd5eac122d015a
Can <zby03772014@hotamil.com>
2026-04-13 22:34:52 +0800

    update 1.6.1

--- LongYinLiZhiZhuan/SmartTrade/Plugin.cs
+++ LongYinLiZhiZhuan/SmartTrade/Plugin.cs
@@ -8,7 +8,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using Object = UnityEngine.Object;
 
-[assembly: MelonInfo(typeof(SmartTrade.Plugin), "SmartTrade", "1.6", "Can")]
+[assembly: MelonInfo(typeof(SmartTrade.Plugin), "SmartTrade", "1.6.1", "Can")]
 [assembly: MelonGame("TppStudio", "LongYinLiZhiZhuan")]
 [assembly: MelonPlatformDomain(MelonPlatformDomainAttribute.CompatibleDomains.IL2CPP)]
 
