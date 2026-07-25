e30eb8d258e7987900e820209004de1dcdf99256
Can <zby03772014@hotamil.com>
2026-07-26 04:06:12 +0800

    update

--- GodWorld/ZaoHuaBMod/Main.cs
+++ GodWorld/ZaoHuaBMod/Main.cs
@@ -1,31 +1,33 @@
 using System.IO;
 using System.Reflection;
-using BepInEx;
-using BepInEx.Unity.Mono;
-using HarmonyLib;
+using MelonLoader;
 using UnityEngine;
 using ZaoHuaBMod.GuiFramework.Config;
 using ZaoHuaBMod.GuiFramework.Localization;
 using ZaoHuaBMod.GuiFramework.Logger;
 using ZaoHuaBMod.GuiFramework.Logger.Adapters;
 
+[assembly: MelonInfo(typeof(ZaoHuaBMod.Main), "ZaoHuaMod", "1.0.0", "Can")]
+[assembly: MelonGame("XiaMenLeiYun", "Zaohua")]
+[assembly: MelonPlatformDomain(MelonPlatformDomainAttribute.CompatibleDomains.MONO)]
 namespace ZaoHuaBMod
 {
-    [BepInPlugin(ModInfo.Guid, ModInfo.Name, ModInfo.Version)]
-    public class Main : BaseUnityPlugin
+    public class Main : MelonMod
     {
         public static Main Instance;
 
-        private void Start()
+        public override void OnInitializeMelon()
         {
             Instance = this;
-            Harmony.CreateAndPatchAll(typeof(Main));
+            var harmony = new HarmonyLib.Harmony("ZHMod1");
+            harmony.PatchAll(typeof(Main));
             
             // 初始化日志：
-            // BepInEx 入口用 new Adapters.BepInExLogger(Logger)
-            // MelonLoader 入口用 new Adapters.MelonLoggerAdapter()
-            // 不初始化则默认走 UnityDebugLogger（Unity Console）
-            Log.Initialize(new BepInExLogger(Logger));
+            // 1.不初始化则默认走 UnityDebugLogger（Unity Console）
+            // 2.BepInEx 入口用 new Adapters.BepInExLogger(Logger)
+            // Log.Initialize(new BepInExLogger(Logger));
+            // 3.MelonLoader 入口用 new Adapters.MelonLoggerAdapter()
+            Log.Initialize(new MelonLoggerAdapter());
             Log.Info("ZaoHuaBMod Loaded!");
 
             // 初始化 Mod 目录与配置
@@ -37,10 +39,67 @@ namespace ZaoHuaBMod
             BaseConfig.ApplyToManager();
             Loc.TryApplyLanguage(BaseConfig.Language);
 
-            GameObject uiObj = new GameObject("ModUI");
-            UnityEngine.Object.DontDestroyOnLoad(uiObj);
-            uiObj.AddComponent<MainView>();
-
+           
+        }
+        private GameObject _uiObj;
+        public override void OnSceneWasLoaded(int buildIndex, string sceneName)
+        {
+            if (_uiObj == null)
+            {
+                _uiObj = new GameObject("ModUI");
+                UnityEngine.Object.DontDestroyOnLoad(_uiObj);
+                _uiObj.AddComponent<MainView>();
+                Log.Info("ModUI 已在场景加载后创建");
+            }
         }
     }
 }
+
+// bepinex
+
+// using System.IO;
+// using System.Reflection;
+// using BepInEx;
+// using BepInEx.Unity.Mono;
+// using HarmonyLib;
+// using UnityEngine;
+// using ZaoHuaBMod.GuiFramework.Config;
+// using ZaoHuaBMod.GuiFramework.Localization;
+// using ZaoHuaBMod.GuiFramework.Logger;
+// using ZaoHuaBMod.GuiFramework.Logger.Adapters;
+//
+// namespace ZaoHuaBMod
+// {
+//     [BepInPlugin(ModInfo.Guid, ModInfo.Name, ModInfo.Version)]
+//     public class Main : BaseUnityPlugin
+//     {
+//         public static Main Instance;
+//
+//         private void Start()
+//         {
+//             Instance = this;
+//             Harmony.CreateAndPatchAll(typeof(Main));
+//             
+//             // 初始化日志：
+//             // BepInEx 入口用 new Adapters.BepInExLogger(Logger)
+//             // MelonLoader 入口用 new Adapters.MelonLoggerAdapter()
+//             // 不初始化则默认走 UnityDebugLogger（Unity Console）
+//             Log.Initialize(new BepInExLogger(Logger));
+//             Log.Info("ZaoHuaBMod Loaded!");
+//
+//             // 初始化 Mod 目录与配置
+//             var modDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+//             Loc.ModDirectory = modDir;
+//             Loc.ScanLanguages();
+//
+//             BaseConfig.Load();
+//             BaseConfig.ApplyToManager();
+//             Loc.TryApplyLanguage(BaseConfig.Language);
+//
+//             GameObject uiObj = new GameObject("ModUI");
+//             UnityEngine.Object.DontDestroyOnLoad(uiObj);
+//             uiObj.AddComponent<MainView>();
+//
+//         }
+//     }
+// }
